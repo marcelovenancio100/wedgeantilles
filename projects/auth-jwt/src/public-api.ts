b7b3be1a4945be3e01da046bd7b4c8261b2e3ce4
Marcelo venancio <marcelovenancio100@users.noreply.github.com>
2019-07-16 11:42:55 -0300

    Update public-api.ts

--- projects/auth-jwt/src/public-api.ts
+++ projects/auth-jwt/src/public-api.ts
@@ -5,3 +5,7 @@
 export * from './lib/auth-jwt.service';
 export * from './lib/auth-jwt.component';
 export * from './lib/auth-jwt.module';
+export * from './lib/auth-jwt.guard';
+export * from './lib/token/token-interceptor';
+export * from './lib/components/login/login.component';
+export * from './lib/components/logout/logout.component';
